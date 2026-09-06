814a8ce558c88661561adaf493b153b77ef48218
ongsirhoong931 <ongsirhoong931@gmail.com>
2026-09-06 12:16:11 +0800

    Fix Terraform formatting

--- infra/envs/sandbox/providers.tf
+++ infra/envs/sandbox/providers.tf
@@ -3,9 +3,9 @@ terraform {
 
   required_providers {
     aws = {
-  source  = "hashicorp/aws"
-  version = "~> 5.0"
-}
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.6"
